c8dc92106986bea5797ac5cc9e5e1d8564d9750d
Pirate Radio Show <75942561+Oscuras98@users.noreply.github.com>
2021-09-23 20:56:23 +0200

    Add files via upload

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,5 +1,5 @@
 function randombg(){
-  var random= Math.floor(Math.random() * 14) + 0;
+  var random= Math.floor(Math.random() * 10) + 0;
   var bigSize = ["url('../static/img/gifs/1.gif')",
                  "url('../static/img/gifs/2.gif')",
                  "url('../static/img/gifs/3.gif')",
@@ -9,10 +9,6 @@ function randombg(){
                  "url('../static/img/gifs/7.gif')",
 		 "url('../static/img/gifs/8.GIF')",
 		 "url('../static/img/gifs/9.GIF')",
-		 "url('../static/img/gifs/10.GIF')",
-		 "url('../static/img/gifs/11.GIF')",
-		 "url('../static/img/gifs/12.GIF')",
-		 "url('../static/img/gifs/13.GIF')",
-		 "url('../static/img/gifs/14.GIF')",];
+		 "url('../static/img/gifs/10.GIF')",];
   document.getElementById("boxes").style.backgroundImage=bigSize[random];
 }
